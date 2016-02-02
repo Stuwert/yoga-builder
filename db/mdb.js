e7441fac350fa59653b56e2e2aeabd3e4dbfd2e3
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-02 12:56:39 -0700

    testing db in cloud

--- db/mdb.js
+++ db/mdb.js
@@ -1,13 +1,14 @@
-
+var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var Schema = mongoose.Schema;
-var url = "localhost:27017s";
+var url = "mongodb://heroku_2s0cn3r5:8rvn5mhr5j73gb28mrt7oiaqki@ds055545.mongolab.com:55545/heroku_2s0cn3r5";
+//var url = "mongodb://localhost:27017";
 MongoClient.connect(url, function(err, dbx) {
     assert.equal(null, err);
     console.log("Connected to db server");
-    dbx.close();
+    // dbx.close();
 })
 
 var mongoose = require('mongoose');
@@ -16,14 +17,14 @@ mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
+    console.log("yo!");
     var posesSchema = mongoose.Schema({
-        pose_id: Schema.ObjectID,
         pose_name: {type: String, trim: true},
         sanskrit_name: {type: String, trim: true},
-        translation: [];
-        category: [];
+        translation: [],
+        category: [],
         difficulty: {type: String, trim: true},
-        description: [];
+        description: [],
         benefits: {type: String, trim: true}
     });
     var Pose = mongoose.model('Pose', posesSchema);
@@ -36,9 +37,17 @@ db.once('open', function() {
          'āsana = posture'],
          category: [
   'Standing',
-  'Balancing',
+  'Balancing'],
 difficulty: 'Expert',
 description:
  [ 'From', 'chair_twist.html', 'pose, the lower arm reaches back around the legs as the upper arm wraps around the back and the fingers of the respective hands eventually meet and interlace.  One foot stays rooted into the earth and straightens while the opposite leg comes up with a bent knee.  Once you are standing upright extend the leg towards the sky.  The ribcage is lifted and the heart is open in the full expression of the pose.  The gaze is forward.' ],
 benefits: 'Increases the flexibility of the spine and back and stretches the shoulders.  Strengthens the legs.  Increases flexibility of the hip and knee joints.  Improves balance.  Opens the groin.  Stretches the hamstrings.'});
+testPose.save(function (err, testPose) {
+  if (err) return console.error(err);
+  console.log("in baby in!");
+});
+Pose.find(function (err, poses) {
+  if (err) return console.error(err);
+  console.log(poses);
+})
 });
