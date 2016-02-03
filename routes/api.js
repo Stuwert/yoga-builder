749c0fd824c9fa3d544851c92de503ff65ed2976
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-03 16:36:15 -0700

    seeeeed me

--- routes/api.js
+++ routes/api.js
@@ -8,40 +8,38 @@ router.get('/', function (req, res, next) {
     //     res.header("Access-Control-Allow-Origin", "*");
     //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     //     res.json(result);
-    // poses.forEach(function(elem) {
-    //     var insertObj = {
-    //         'pose_name': 0,
-    //         'sanskrit_name': 0,
-    //         'translation': 0,
-    //         'category': 0,
-    //         'difficulty': 0,
-    //         'description': 0,
-    //         'benefits': 0,
-    //         'benefits_array': 0
-    //     }
-    //     var catBox = elem.category.split(" / ");
-    //     catObj = [];
-    //     catBox.forEach(function(elem) {
-    //         var temp = elem.split(" ").join("_");
-    //         catObj.push(temp);
-    //     });
-    //     var benArr = elem.benefits.split(" ");
-    //     var diffObj = elem.difficulty;
-    //     //boom
-    //     insertObj.pose_name = elem.pose_name;
-    //     insertObj.sanskrit_name = elem.sanskrit_name;
-    //     insertObj.translation = JSON.stringify(elem.translation);
-    //     insertObj.category = catObj;
-    //     insertObj.difficulty = diffObj;
-    //     insertObj.description = JSON.stringify(elem.description);
-    //     insertObj.benefits = JSON.stringify(elem.benefits);
-    //     insertObj.benefits_array = JSON.stringify(benArr);
-    //     database.addPose(insertObj).then(function (result) {
-    //             res.send("...");
-    //     })
-    // })
-    //})
-res.send("hi");
+    //        })
+    poses.forEach(function(elem) {
+        var insertObj = {
+            'pose_name': 0,
+            'sanskrit_name': 0,
+            'translation': 0,
+            'category': 0,
+            'difficulty': 0,
+            'description': 0,
+            'benefits': 0,
+            'benefits_array': 0
+        }
+        var catBox = elem.category.split(" / ");
+        catObj = [];
+        catBox.forEach(function(elem) {
+            var temp = elem.split(" ").join("_");
+            catObj.push(temp);
+        });
+        var benArr = elem.benefits.split(" ");
+        var diffObj = elem.difficulty;
+        insertObj.pose_name = elem.pose_name;
+        insertObj.sanskrit_name = elem.sanskrit_name;
+        insertObj.translation = JSON.stringify(elem.translation);
+        insertObj.category = catObj;
+        insertObj.difficulty = diffObj;
+        insertObj.description = JSON.stringify(elem.description);
+        insertObj.benefits = JSON.stringify(elem.benefits);
+        insertObj.benefits_array = JSON.stringify(benArr);
+        database.addPose(insertObj).then(function (result) {
+                res.send("...");
+        })
+    })
 })
 
 router.get('/poses/cat', function(req, res, next) {
