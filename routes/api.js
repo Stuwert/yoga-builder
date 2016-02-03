e81bea78595bd0666c524c82cb749c2ebcbaa944
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-03 15:08:34 -0700

    aflakjf

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,9 @@ var database = require('../db/database');
 var poses = require('../seedposes.js');
 router.get('/', function (req, res, next) {
     database.outputAll().then(function(result){
-        res.send(JSON.stringify(result));
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.json(result);
     })
 
 })
@@ -15,9 +17,8 @@ router.get('/', function (req, res, next) {
 //     })
 //
 //     router.get('/poses', function (req, res, next) {
-//         res.header("Access-Control-Allow-Origin", "*");
-//         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//
+
+
 // })
 
 
