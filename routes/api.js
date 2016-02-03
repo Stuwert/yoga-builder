07696de3c7585d283efe4d1d741156103fec17e7
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-02 17:26:18 -0700

    klsla

--- routes/api.js
+++ routes/api.js
@@ -18,13 +18,13 @@ router.get('/all', function (req, res, next) {
               if (err) {
                   return console.error(err);
               } else {
-                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+
                   res.format({
-                      //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
+
                     html: function(){
                         res.send(JSON.stringify(poses));
                     },
-                    //JSON response will show all blobs in JSON format
+                
                     json: function(){
                         res.json(poses);
                     }
