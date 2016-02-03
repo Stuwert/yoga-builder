d4a254d1c16e947728aed2894c01282aff19e193
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-03 14:55:25 -0700

    aflakjf

--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,7 @@ var express = require('express');
 router = express.Router();
 var knex = require('../db/knex');
 var database = require('../db/database');
-bodyParser = require('body-parser');
-methodOverride = require('method-override');
-router.use(bodyParser.urlencoded({ extended: true }))
 var poses = require('../seedposes.js');
-
-
-
-router.use(methodOverride(function(req, res){
-     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-     var method = req.body._method
-     delete req.body._method
-     return method
-          }
-    }))
 router.get('/api', function (req, res, next) {
     // poses.forEach(function(elem) {
     //     var insertObj = {
