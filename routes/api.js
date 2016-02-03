26d974fee8cbe19b2b4037e3710e64a76ce1f537
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-03 10:31:07 -0700

    cat11

--- routes/api.js
+++ routes/api.js
@@ -34,6 +34,8 @@ router.get('/all', function (req, res, next) {
     });
 
     router.get('/poses', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
                 var query = req.query.category;
                 var payload = query.split(" ");
                 mongoose.model('Pose').find({category: {$in: payload}}, function (err, poses) {
