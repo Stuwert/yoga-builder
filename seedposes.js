f0ed69cb2895b22ef8a220b2e32cbeadbf47c1b1
Patrick Beam <patrick.j.beam@gmail.com>
2016-02-03 15:04:09 -0700

    aflakjf

--- seedposes.js
+++ seedposes.js
@@ -1448,4 +1448,36 @@ var poses = [ { file_reference: 'bird_of_paradise_revolved.html',
     description: [ 'From a supine position lying on your back, pull one knee into the chest with the hands clasped around the bent knee to the level of pressure desired (gas trapped in the large intestine may be released in this asana).  The other leg is extended straight and the gaze is natural and forward.' ],
     benefits: 'Releases gas trapped in large intestine.  Stretches the cervical spine (neck).  Improves the digestion system.  Aides in elimination.' } ]
 
+    poses.forEach(function(elem) {
+        var insertObj = {
+            'pose_name': 0,
+            'sanskrit_name': 0,
+            'translation': 0,
+            'category': 0,
+            'difficulty': 0,
+            'description': 0,
+            'benefits': 0,
+            'benefits_array': 0
+        }
+        var catBox = elem.category.split(" / ");
+        catObj = [];
+        catBox.forEach(function(elem) {
+            var temp = elem.split(" ").join("_");
+            catObj.push(temp);
+        });
+        var benArr = elem.benefits.split(" ");
+        var diffObj = elem.difficulty.split(" / ");
+        insertObj.pose_name = elem.pose_name;
+        insertObj.sanskrit_name = elem.sanskrit_name;
+        insertObj.translation = JSON.stringify(elem.translation);
+        insertObj.category = catObj;
+        insertObj.difficulty = diffObj;
+        insertObj.description = JSON.stringify(elem.description);
+        insertObj.benefits = JSON.stringify(elem.benefits);
+        insertObj.benefits_array = JSON.stringify(benArr);
+        database.addPose(insertObj).then(function (result) {
+                res.send("...");
+        })
+    })
+
 module.exports = poses;
